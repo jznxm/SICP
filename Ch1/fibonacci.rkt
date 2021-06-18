d0a1eb524a1341bbed82e395a4c58b97c3656a47
jznxm <keithkun93@gmail.com>
2021-06-18 09:34:23 +0800

    Add fibonacci.rkt

---
+++ Ch1/fibonacci.rkt
@@ -0,0 +1,18 @@
+#lang sicp
+
+(define (fib1 n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib1 (- n 1))
+                 (fib1 (- n 2))))))
+
+(define (fib2 n)
+  (fib-iter 1 0 n))
+
+(define (fib-iter a b count)
+  (if (= count 0)
+      b
+      (fib-iter (+ a b) a (- count 1))))
+
+(fib1 20)
+(fib2 20)

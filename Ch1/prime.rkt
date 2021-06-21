3af75779f5d3b389c8fb17f5c2a7e9fad87063f8
jznxm <keithkun93@gmail.com>
2021-06-21 09:29:46 +0800

    Add prime.rkt

---
+++ Ch1/prime.rkt
@@ -0,0 +1,23 @@
+#lang sicp
+
+(define (smallest-division n)
+  (find-divisor n 2))
+
+(define (square n) (* n n))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-division n)))
+
+(prime? 17)
+(prime? 42)
+(prime? 1024)
+
+                    

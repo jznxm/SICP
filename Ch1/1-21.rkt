4e719c99ecef347c2c126170acf726cc92f59e8b
jznxm <keithkun93@gmail.com>
2021-06-21 09:35:26 +0800

    Add 1-21.rkt

---
+++ Ch1/1-21.rkt
@@ -0,0 +1,18 @@
+#lang sicp
+
+(define (smallest-division n)
+  (find-divisor n 2))
+
+(define (square n) (* n n))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(smallest-division 199)
+(smallest-division 1999)
+(smallest-division 19999)

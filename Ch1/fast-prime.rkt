cd8e26f41388b4ab9a3369b69f3b56319552d4b5
jznxm <keithkun93@gmail.com>
2021-06-21 09:29:29 +0800

    Add fast-prime.rkt

---
+++ Ch1/fast-prime.rkt
@@ -0,0 +1,27 @@
+#lang sicp
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
+(define (square n) (* n n))
+(define (even? n) (= (remainder n 2) 0))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
+
+(fast-prime? 23457 100)
+(fast-prime? 17 5)
+(fast-prime? 20 4)

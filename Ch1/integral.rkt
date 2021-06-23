6602c99ba6ac753aa828eabfb29030618796e6c2
jznxm <keithkun93@gmail.com>
2021-06-23 09:30:18 +0800

    Add integral.rkt

---
+++ Ch1/integral.rkt
@@ -0,0 +1,18 @@
+#lang sicp
+
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (integral f a b dx)
+  (define (add-dx x) (+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b)
+          dx))
+
+(define (cube n) (* n n n))
+
+(integral cube 0 1 0.01)
+
+(integral cube 0 1 0.001)

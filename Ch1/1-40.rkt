5694f42997f4bd17cc8b91fe65d205c0d84825be
jznxm <keithkun93@gmail.com>
2021-06-25 17:11:39 +0800

    Add 1-40.rkt

---
+++ Ch1/1-40.rkt
@@ -0,0 +1,59 @@
+#lang sicp
+
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+(define dx 0.00001)
+
+(define (square x) (* x x))
+(define (cube x) (* x x x))
+(define (average x y) (/ (+ x y) 2))
+
+;:((deriv cube) 5)
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))))
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+(define (fixed-point-of-transform g transform guess)
+  (fixed-point (transform g) guess))
+
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
+
+(define (sqrt x)
+  (fixed-point-of-transform (lambda (y) (/ x y))
+                            average-damp
+                            1.0))
+
+(define (sqrt1 x)
+  (fixed-point-of-transform (lambda (y) (- (square y) x))
+                            newton-transform
+                            1.0))
+
+;:(sqrt 2000)
+;:(sqrt1 2000)
+
+(define (cubic a b c)
+   (lambda (x)
+     (+ (cube x)
+        (* a (square x))
+        (* b x)
+        c)))
+(newtons-method (cubic 1 2 3) 1)

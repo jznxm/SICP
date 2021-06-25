89e76fc4ec9303161c151fe65f82166d0b4855e5
jznxm <keithkun93@gmail.com>
2021-06-25 17:47:43 +0800

    Update 1-43.rkt

--- Ch1/1-43.rkt
+++ Ch1/1-43.rkt
@@ -9,3 +9,10 @@
         (f ((repeated f (- n 1)) x)))))
 
 ((repeated square 2) 5)
+
+(define (repeated1 f n)
+    (if (= n 1)
+        f
+        (lambda (x) (f ((repeated f (- n 1)) x)))))
+
+((repeated1 square 2) 5)

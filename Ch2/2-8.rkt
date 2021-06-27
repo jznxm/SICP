89be0e3f7ffd2a73c27084c27dfded5a80224991
jznxm <keithkun93@gmail.com>
2021-06-27 19:19:20 +0800

    Add 2-8.rkt

---
+++ Ch2/2-8.rkt
@@ -0,0 +1,42 @@
+#lang sicp
+
+(define (make-interval a b) (cons a b))
+(define (upper-bound interval) (max (car interval) (cdr interval)))
+(define (lower-bound interval) (min (car interval) (cdr interval)))
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (mul-interval x
+                (make-interval (/ 1.0 (upper-bound y))
+                               (/ 1.0 (lower-bound y)))))
+
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
+
+(define (display-interval i) 
+  (display "[") 
+  (display (lower-bound i)) 
+  (display ",") 
+  (display (upper-bound i)) 
+  (display "]")
+  (newline))
+
+(define intera (make-interval 5 6))
+(define interb (make-interval 2 3))
+
+(display-interval (add-interval intera interb))
+(display-interval (mul-interval intera interb))
+(display-interval (div-interval intera interb))
+(display-interval (sub-interval intera interb))

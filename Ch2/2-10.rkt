e21c63b51b68a253dea1c963c6d6fe30be933b7b
jznxm <keithkun93@gmail.com>
2021-06-27 20:08:20 +0800

    Update 2-10.rkt

--- Ch2/2-10.rkt
+++ Ch2/2-10.rkt
@@ -17,7 +17,7 @@
                    (max p1 p2 p3 p4))))
 
 (define (div-interval x y)
-  (if (<= 0 (* (lower-bound y) (upper-bound y))) 
+  (if (>= 0 (* (lower-bound y) (upper-bound y))) 
        (error "Division error (interval spans 0)" y) 
        (mul-interval x  
                      (make-interval (/ 1. (upper-bound y)) 

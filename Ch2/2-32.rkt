23564770177a09cdbf3e678cd7e6b2c8279fe2b7
jznxm <keithkun93@gmail.com>
2021-06-29 21:27:38 +0800

    Add 2-32.rkt

---
+++ Ch2/2-32.rkt
@@ -0,0 +1,9 @@
+#lang sicp
+
+(define (subsets s)
+  (if (null? s)
+      (list nil)
+      (let ((rest (subsets (cdr s))))
+        (append rest (map (lambda (x) (cons (car s) x)) rest)))))
+
+(subsets (list 1 2 3))

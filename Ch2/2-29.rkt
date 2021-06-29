b0ef1444118e23f39ecbdc8ae3f185eaeba2db60
jznxm <keithkun93@gmail.com>
2021-06-29 20:38:46 +0800

    Update 2-20.rkt

--- Ch2/2-29.rkt
+++ Ch2/2-29.rkt
@@ -19,15 +19,33 @@
   (car (cdr branch)))
 
 (define (total-weight mobile)
-  (cond ((null? mobile) 0)
-        ((not (pair? mobile)) mobile)
-        (else (+ (total-weight (branch-structure (left-branch mobile)))
-                 (total-weight (branch-structure (right-branch mobile))))))) 
+  (if (not (pair? mobile))
+      mobile
+      (+ (total-weight (branch-structure (left-branch mobile)))
+         (total-weight (branch-structure (right-branch mobile))))))
 
-(define mobile (make-mobile (make-branch 6 7) (make-branch 9 10)))
+(define mobile (make-mobile 
+                (make-branch 4 6) 
+                (make-branch 5 
+                             (make-mobile 
+                              (make-branch 3 7) 
+                              (make-branch 9 8))))) 
 
 (left-branch mobile)
 (right-branch mobile)
 (branch-length mobile)
 (branch-structure mobile)
 (total-weight mobile)
+
+(define (balanced? m) 
+  (cond ((null? m) #t) 
+        ((not (pair? m)) #t) 
+        (else 
+         (and (= (* (branch-length (left-branch m)) 
+                    (total-weight (branch-structure (left-branch m)))) 
+                 (* (branch-length (right-branch m)) 
+                    (total-weight (branch-structure (right-branch m))))) 
+              (balanced? (branch-structure (left-branch   m))) 
+              (balanced? (branch-structure (right-branch  m)))))))
+
+(balanced? mobile)
